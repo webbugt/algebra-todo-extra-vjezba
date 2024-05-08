f0f636cc8b13af76d3c200c3d96e310a98c8f624
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-08 19:31:53 +0200

    implement Task in TaskList

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,6 @@
 import { useUIDSeed } from "react-uid"
 import PropTypes from 'prop-types'
+import Task from "./Task"
 
 
 const TaskList = ({
@@ -8,7 +9,7 @@ const TaskList = ({
     const seed  = useUIDSeed()
     return <>
         {tasks.map((task,index)=>{
-            return <p key={seed(task,index)} id={seed(task,index)}> {task.title} - {task.status}</p>
+            return <Task key={seed(task,index)} title={task.title} status={task.status} />
         })}
     </>
 }
