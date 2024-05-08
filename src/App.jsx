fa64cf24e15e76afad68fa2a1a0002d92bc88e85
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-08 20:37:53 +0200

    removeTask method

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,29 @@ function App() {
   const addTask = (newTask) => {
     setTasks((previousTasks) => [...previousTasks, newTask]);
   };
+
+  const removeTask = (taskToRemove) => {
+    setTasks(
+      (previousTasks) => {
+        // create list of task titles
+        const taskTitles = previousTasks
+          .map((task) => task.title);
+        // find index of task to delete via title
+        const indexOfTaskToRemove = taskTitles
+          .indexOf(taskToRemove.title);
+        // if it's not found (-1) return previous state
+        if (indexOfTaskToRemove === -1) {
+          return previousTasks;
+        }
+        // create new array for mutation
+        const newTaskList = [...previousTasks];
+        // mutate array via splice(delete element at index)
+        newTaskList.splice(indexOfTaskToRemove, 1);
+        // return new state
+        return newTaskList;
+      },
+    );
+  };
   return (
     <>
       <h1>ToDo App</h1>
@@ -42,6 +65,17 @@ function App() {
         }}
       >
         Add demo task
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          removeTask({
+            title: 'Demo task',
+            status: 'new',
+          });
+        }}
+      >
+        Remove Demo Task
       </button>
       <TaskList tasks={tasks} />
     </>
