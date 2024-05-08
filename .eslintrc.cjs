aed5fc7e7e37e131b4780896cf9bcb163affbd04
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-08 19:48:08 +0200

    install eslint-config-airbnb and add to eslinrtc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,14 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'eslint-config-airbnb'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
