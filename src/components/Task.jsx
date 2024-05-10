f3e747eaed3b0533e29f2a6e20e231b4a835b04d
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-10 20:49:58 +0200

    create TaskButton with variants

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,54 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { ErrorBoundary } from 'react-error-boundary';
 import styles from './Task.module.css';
 import { changeTask, removeTask } from '../store/taskSlice';
+
+const variants = {
+  done: {
+    title: 'Set task as done',
+    content: 'D',
+    className: styles.complete,
+  },
+  reset: {
+    title: 'Reset task',
+    content: 'R',
+    className: styles.reset,
+  },
+  'in-progress': {
+    title: 'Set task in progress',
+    content: 'I',
+    className: styles['in-progress'],
+  },
+  remove: {
+    title: 'Remove task',
+    content: 'X',
+    className: styles.remove,
+  },
+};
+
+function TaskButton({ variant, onClick }) {
+  const currentVariant = variants[variant];
+  if (!currentVariant) {
+    throw new Error(`Couldn't find variant name ${variant}`);
+  }
+  const { className, content, title } = currentVariant;
+  return (
+    <button
+      className={className}
+      type="button"
+      title={title}
+      onClick={onClick}
+    >
+      {content}
+    </button>
+  );
+}
+
+TaskButton.propTypes = {
+  variant: PropTypes.oneOf(Object.keys(variants)).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 function Task({
   title, status, id,
@@ -16,63 +63,45 @@ function Task({
   };
 
   return (
-    <div className={styles.task}>
-      <span className="title">{title}</span>
-      {' '}
-      <span className={styles.status}>{status}</span>
-      <div className={styles.controls}>
-        {/* Done button */}
-        {status !== 'done' && (
-        <button
-          className={styles.complete}
-          type="button"
-          title="Set task as done"
-          onClick={() => {
-            changeHandler({ title, status: 'done', id });
-          }}
-        >
-          C
-        </button>
-        )}
-        {/* Reset button */}
-        {status === 'done' && (
-        <button
-          className={styles.reset}
-          type="button"
-          title="Reset task"
-          onClick={() => {
-            changeHandler({ title, status: 'new', id });
-          }}
-        >
-          R
-        </button>
-        )}
-        {/* In progress button */}
-        {status !== 'in-progress' && (
-        <button
-          className={styles['in-progress']}
-          type="button"
-          title="Set task in progress"
-          onClick={() => {
-            changeHandler({ title, status: 'in-progress', id });
-          }}
-        >
-          I
-        </button>
-        )}
-        {/* Delete button */}
-        <button
-          className={styles.delete}
-          type="button"
-          title="Remove task"
-          onClick={() => {
-            removeHandler({ title, id });
-          }}
-        >
-          X
-        </button>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <div className={styles.task}>
+        <span className="title">{title}</span>
+        {' '}
+        <span className={styles.status}>{status}</span>
+        <div className={styles.controls}>
+          {status !== 'done' && (
+          <TaskButton
+            variant="done"
+            onClick={() => {
+              changeHandler({ title, status: 'done', id });
+            }}
+          />
+          )}
+          {status !== 'new' && (
+          <TaskButton
+            variant="reset"
+            onClick={() => {
+              changeHandler({ title, status: 'new', id });
+            }}
+          />
+          )}
+          {status !== 'in-progress' && (
+          <TaskButton
+            variant="in-progress"
+            onClick={() => {
+              changeHandler({ title, status: 'in-progress', id });
+            }}
+          />
+          )}
+          <TaskButton
+            variant="remove"
+            onClick={() => {
+              removeHandler({ title, id });
+            }}
+          />
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
