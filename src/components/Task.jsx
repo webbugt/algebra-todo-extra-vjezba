1dd8d021aaeea60385c23d24f65c8cdcdaf99b86
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-05-10 20:16:55 +0200

    add reset button and conditionally show buttons based on status

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -21,27 +21,50 @@ function Task({
       {' '}
       <span className={styles.status}>{status}</span>
       <div className={styles.controls}>
+        {/* Done button */}
+        {status !== 'done' && (
         <button
           className={styles.complete}
           type="button"
+          title="Set task as done"
           onClick={() => {
             changeHandler({ title, status: 'done', id });
           }}
         >
           C
         </button>
+        )}
+        {/* Reset button */}
+        {status === 'done' && (
+        <button
+          className={styles.reset}
+          type="button"
+          title="Reset task"
+          onClick={() => {
+            changeHandler({ title, status: 'new', id });
+          }}
+        >
+          R
+        </button>
+        )}
+        {/* In progress button */}
+        {status !== 'in-progress' && (
         <button
           className={styles['in-progress']}
           type="button"
+          title="Set task in progress"
           onClick={() => {
             changeHandler({ title, status: 'in-progress', id });
           }}
         >
           I
         </button>
+        )}
+        {/* Delete button */}
         <button
           className={styles.delete}
           type="button"
+          title="Remove task"
           onClick={() => {
             removeHandler({ title, id });
           }}
